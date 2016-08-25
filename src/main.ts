dab06b0a92ceb71e75a116f737a999e5f97406a7
Hagai Cohen <hagai.co@gmail.com>
2016-08-26 01:35:45 +0300

    Added session

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Schema } from "./schema";
 import * as cors from "cors";
 import * as helmet from "helmet";
 import * as morgan from "morgan";
+import * as session from "express-session";
 
 // Either to export GraphiQL (Debug Interface) or not.
 const NODE_ENV = process.env.NODE_ENV !== "production" ? "dev" : "production";
@@ -13,6 +14,15 @@ const EXPORT_GRAPHIQL = NODE_ENV !== "production";
 const PORT = process.env.PORT || 3000;
 // Enable cors (cross-origin HTTP request) or not.
 const ENABLE_CORS = NODE_ENV !== "production";
+const SESSION_OPTS = {
+    cookie: { maxAge: 60000, secure: false },
+    resave: false,
+    saveUninitialized: false,
+    secret: "Sup3rH4rdP4$$w0rd",
+};
+if ( "production" === NODE_ENV ) {
+    SESSION_OPTS.cookie.secure = true;
+};
 
 const GRAPHQL_ROUTE = "/graphql";
 const GRAPHIQL_ROUTE = "/graphiql";
@@ -23,10 +33,15 @@ app.use(morgan(NODE_ENV));
 if ( true === ENABLE_CORS ) {
     app.use(GRAPHQL_ROUTE, cors());
 }
+app.use(session(SESSION_OPTS));
 
-app.use(GRAPHQL_ROUTE, bodyParser.json(), apolloExpress({
-    schema: Schema,
+app.use(GRAPHQL_ROUTE, bodyParser.json(), apolloExpress(function(req, res) {
+    console.log(req.session);
+    return {
+        schema: Schema,
+    };
 }));
+
 if ( true === EXPORT_GRAPHIQL ) {
     app.use(GRAPHIQL_ROUTE, graphiqlExpress({
         endpointURL: GRAPHQL_ROUTE,
