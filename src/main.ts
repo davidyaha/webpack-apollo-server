bc520808d1bb2009700f56f71b86634ebe182c42
Hagai Cohen <hagai.co@gmail.com>
2016-08-26 23:06:13 +0300

    Added basic authentication concept

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,17 @@ const EXPORT_GRAPHIQL = NODE_ENV !== "production";
 const PORT = process.env.PORT || 3000;
 // Enable cors (cross-origin HTTP request) or not.
 const ENABLE_CORS = NODE_ENV !== "production";
+// This is just for the demo, use a decent store please :)
+const USER_STORE = {
+    password: "1234",
+    username: "auth-demo",
+};
 const SESSION_OPTS = {
     cookie: { maxAge: 60000, secure: false },
     resave: false,
     saveUninitialized: false,
+    // Please, don't store your secrets in the code.
+    // use environment variables or configuration file which is not in git. ;)
     secret: "Sup3rH4rdP4$$w0rd",
 };
 if ( "production" === NODE_ENV ) {
@@ -26,6 +33,17 @@ if ( "production" === NODE_ENV ) {
 
 const GRAPHQL_ROUTE = "/graphql";
 const GRAPHIQL_ROUTE = "/graphiql";
+
+// TODO: Find a decent users/accounts library, and replace it with it.
+// passport doesn't support authenticating without being a middlewere.
+const loginMethod = (username, password) => {
+    console.log(`Authenticating ${username}`);
+    if ( (username !== USER_STORE.username) ||
+         (password !== USER_STORE.password ) ) {
+        throw new Error("Incorrect Credentials");
+    }
+    return { username: username };
+};
 
 let app = express();
 app.use(helmet());
@@ -36,8 +54,33 @@ if ( true === ENABLE_CORS ) {
 app.use(session(SESSION_OPTS));
 
 app.use(GRAPHQL_ROUTE, bodyParser.json(), apolloExpress(function(req, res) {
-    console.log(req.session);
     return {
+        context: {
+            isAuthenticated: () => {
+                return req.session.hasOwnProperty("userInfo");
+            },
+            login: (username, password) => {
+                /* tslint:disable:no-string-literal */
+                req.session["userInfo"] = loginMethod(username, password);
+                return "success";
+            },
+            logout: () => {
+                if ( undefined === req.session ) {
+                    return Promise.resolve("success");
+                }
+
+                return new Promise((resolve, reject) => {
+                    req.session.destroy((err) => {
+                        if ( err ) {
+                            reject(err);
+                            return;
+                        }
+                        resolve("success");
+                    });
+                });
+            },
+            session: req.session,
+        },
         schema: Schema,
     };
 }));
--- src/schema.ts
+++ src/schema.ts
@@ -13,10 +13,56 @@ const QueryType = new GraphQLObjectType({
                 return "it works";
             },
         },
+        topSecretString: {
+            type: GraphQLString,
+            resolve(root, args, ctx) {
+                if ( false === ctx.isAuthenticated() ) {
+                    throw new Error("Permession denied");
+                }
+
+                return "it works, but only after login!";
+            },
+        },
     },
     name: "QueryRoot",
 });
 
+const MutationType = new GraphQLObjectType({
+    description: "Root Mutation",
+    fields: {
+        login: {
+            args: {
+                password: { // NEVER SEND OR SEND PASSWORDS PLAIN! NEVER!
+                    type: GraphQLString,
+                },
+                username: {
+                    type: GraphQLString,
+                },
+            },
+            type: GraphQLString,
+            resolve(root: undefined, args: any, ctx) {
+                if ( undefined === args.username ) {
+                    throw new Error("Username is missing");
+                }
+                if ( undefined === args.password ) {
+                    throw new Error("Password is missing");
+                }
+
+                return ctx.login(args.username, args.password);
+            },
+        },
+        logout: {
+            args: {},
+            type: GraphQLString,
+            resolve(root: undefined, args: any, ctx) {
+                return ctx.logout();
+            },
+        },
+    },
+    name: "MutationType",
+});
+
 export const Schema = new GraphQLSchema({
+    mutation: MutationType,
     query: QueryType,
 });
